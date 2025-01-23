ebc5232b0d6f27cd49ebb8d8d88c9112956ee835
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2025-01-23 18:40:01 +0000

    Get game by id

--- src/main/java/com/mayara/dslist/model/controllers/GameController.java
+++ src/main/java/com/mayara/dslist/model/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.mayara.dslist.model.controllers;
 
+import com.mayara.dslist.model.dto.GameDTO;
 import com.mayara.dslist.model.dto.GameMinDTO;
 import com.mayara.dslist.model.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,9 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll(){
         return gameService.findAll();
+    }
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+       return gameService.findById(id);
     }
 }
--- src/main/java/com/mayara/dslist/model/service/GameService.java
+++ src/main/java/com/mayara/dslist/model/service/GameService.java
@@ -1,11 +1,14 @@
 package com.mayara.dslist.model.service;
+import com.mayara.dslist.model.dto.GameDTO;
 import com.mayara.dslist.model.dto.GameMinDTO;
 import com.mayara.dslist.model.entities.Game;
 import com.mayara.dslist.model.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
@@ -13,8 +16,16 @@ public class GameService {
     GameRepository gameRepository;
 
     //stream= vai lib que permite operações com sequencia de dados
+    //transactional, é uma boa prática ACID
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result= gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        //depois posso fazer o tratamento com o optional
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
     }
 }
