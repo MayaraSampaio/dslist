274797bd280dcdb24a1d7096ce02412ac6a87c3d
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2025-01-24 18:30:38 +0000

    Run homolog

--- src/main/java/com/mayara/dslist/projections/GameMinProjection.java
+++ src/main/java/com/mayara/dslist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.mayara.dslist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
